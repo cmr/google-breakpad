d599da01c627368fab5fe6e6940b449b8b08fb91
Corey Richardson <corey@octayn.net>
2014-08-16 04:01:33 -0400

    Linux: C shims for Rust

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
 # should have been executed by sh
+
+if [ $(uname) != "Linux" ]; then
+    echo "Rust breakpad glue not supported on non-Linux platforms!"
+    exit 1
+fi
+
 ./configure
 make -j4
-cp src/libbreakpad.a $OUT_DIR
+if [ $CXX = "" ]; then
+    CXX = c++
+fi
+"$CXX" $CXXFLAGS -O -static
+clang++ -c -static -pthread -I src rust_breakpad_linux.cc
+ar rcs librust_breakpad_client.a rust_breakpad_linux.o
+ar rcx librust_breakpad_client.a src/client/linux/libbreakpad_client.a
+
+cp librust_breakpad_client.a "$OUT_DIR"
---
+++ rust_breakpad_linux.cc
@@ -0,0 +1,78 @@
+#include "client/linux/handler/exception_handler.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace google_breakpad;
+
+static bool dumpCallback(const google_breakpad::MinidumpDescriptor& descriptor,
+        void* context,
+        bool succeeded)
+{
+    printf("Dump path: %s\n", descriptor.path());
+    return succeeded;
+}
+
+typedef ExceptionHandler::MinidumpCallback MinidumpCallback;
+typedef ExceptionHandler::FilterCallback FilterCallback;
+
+struct WrapperContext {
+    MinidumpCallback mcb;
+    FilterCallback fcb;
+    void *real_context;
+
+    WrapperContext(FilterCallback fcb, MinidumpCallback mcb, void *context) {
+        this->mcb = mcb;
+        this->fcb = fcb;
+        this->real_context = context;
+    }
+};
+
+static bool filter_callback_wrapper(void *context) {
+    WrapperContext *cont = reinterpret_cast<WrapperContext*>(context);
+    return cont->fcb(cont->real_context);
+}
+
+static bool minidump_callback_wrapper(const MinidumpDescriptor &desc, void *context, bool succeeded) {
+    WrapperContext *cont = reinterpret_cast<WrapperContext*>(context);
+    return cont->mcb(desc, cont->real_context, succeeded);
+}
+
+extern "C" {
+    void *rust_breakpad_descriptor_new(const char *path) {
+        MinidumpDescriptor *desc = new MinidumpDescriptor(path);
+        return reinterpret_cast<void*>(desc);
+    }
+
+    const char *rust_breakpad_descriptor_path(const void *desc_) {
+        MinidumpDescriptor *desc = reinterpret_cast<MinidumpDescriptor*>(desc_);
+        return desc->path();
+    }
+
+    void rust_breakpad_descriptor_free(void *desc) {
+        delete reinterpret_cast<MinidumpDescriptor*>(desc);
+    }
+
+    void *rust_breakpad_exceptionhandler_new(void *desc, FilterCallback fcb,
+            MinidumpCallback mcb, void *context, int install_handler) {
+
+        WrapperContext *wrapper_context = new WrapperContext(fcb, mcb, context);
+
+        ExceptionHandler *eh = new ExceptionHandler(
+            *reinterpret_cast<MinidumpDescriptor*>(desc),
+            filter_callback_wrapper,
+            minidump_callback_wrapper,
+            reinterpret_cast<void*>(wrapper_context),
+            install_handler,
+            -1);
+        return reinterpret_cast<void*>(eh);
+    }
+
+    int rust_breakpad_exceptionhandler_write_minidump(void *eh) {
+        ExceptionHandler *exch = reinterpret_cast<ExceptionHandler*>(eh);
+        return exch->WriteMinidump();
+    }
+
+    void rust_breakpad_exceptionhandler_free(void *eh) {
+        delete reinterpret_cast<ExceptionHandler*>(eh);
+    }
+}
